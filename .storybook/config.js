4a14bb980ac8fc12e341210922289b5a651c1a59
marcspicer <marc@marcspicer.co>
2017-12-22 16:03:30 -0500

    footer added in stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon } from '@storybook/react';
+import infoAddon from '@storybook/addon-info';
 
+setAddon(infoAddon);
 function loadStories() {
   require('../src/stories');
 }
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,16 +3,30 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import Logo from "../components/logo/logo";
 import Nav from "../components/navbar/navbar";
-import Header from "../components/header/header"
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 import { BrowserRouter } from 'react-router-dom';
 
+const headerStories = storiesOf("Header", module);
+const footerStories = storiesOf("Footer", module);
 
-storiesOf("Header", module)
-.addDecorator(story=>(<BrowserRouter initialEntries={['/']}>{story()}</BrowserRouter>))
+headerStories.addDecorator(withKnobs);
+headerStories.addDecorator(story=>(<BrowserRouter initialEntries={['/']}>{story()}</BrowserRouter>));
+
+footerStories.addDecorator(withKnobs);
+footerStories.addDecorator(story=>(<BrowserRouter initialEntries={['/']}>{story()}</BrowserRouter>));
+
+headerStories
 .add("Logo", ()=> <Logo />)
 .add("Navbar", ()=><Nav/>)
-.add("complete", ()=><Header/>)
+.add("complete", ()=><Header/>);
+
+footerStories
+.addWithInfo("Footer", "Static Footer", ()=>{
+  return <Footer/>
+})
